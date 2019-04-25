b3c4bd4407bd5891fd0105b457eefa55d3803481
gustavo@mumuki.org <gustavo@mumuki.org>
2019-04-25 19:08:08 +0000

    Mumuki Export on 2019-04-25 19:08:08 +0000

--- 00010_Precedencia/extra.py
+++ 00010_Precedencia/extra.py
@@ -1,13 +1,8 @@
 def cuotas(tarjeta):
-  switch(tarjeta):
-    case "visa":
-      return 6
-    case "mastercard":
-      return 2
-    default:
-      return 1
-
-
+  return {
+    'visa': 6,
+    'mastercard': 2,
+  }.get(tarjeta, 1)
 
 def paga_con_tarjeta(se_cobra_interes, tarjeta, efectivo_disponible):
   return not se_cobra_interes and cuotas(tarjeta) >= 3 or efectivo_disponible < 100
