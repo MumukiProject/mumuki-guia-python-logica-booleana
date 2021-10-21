b0ecbbd44df1fae4f3f88ee864cfae407348ec96
gustavo@mumuki.org <gustavo@mumuki.org>
2021-10-21 21:24:21 +0000

    Mumuki Export on 2021-10-21 21:24:21 +0000

--- 00007_Claroscuro/test.py
+++ 00007_Claroscuro/test.py
@@ -1,16 +1,12 @@
   
-  def test_arcadio_y_aureliano_jose_son_medios_hermanos(self):
-    self.assertTrue(son_medio_hermanos(arcadio, aureliano_jose))
-  
-  def test_aureliano_segundo_y_remedios_no_son_medios_hermanos(self):
-    self.assertFalse(son_medio_hermanos(aureliano_segundo, remedios))
-  
-  def test_aureliano_segundo_y_aureliano_jose_no_son_medios_hermanos(self):
-    self.assertFalse(son_medio_hermanos(aureliano_segundo, aureliano_jose))
-  
-  def test_remedios_y_arcadio_no_son_medios_hermanos(self):
-    self.assertFalse(son_medio_hermanos(remedios, arcadio))
-    
-  def test_felipe_perez_y_martin_perez_son_medios_hermanos(self):
-    self.assertTrue(son_medio_hermanos(felipe_perez, martin_perez))
+  def test_tiene_contraste_blanco_negro_es_verdadero(self):
+    self.assertTrue(tiene_contraste("blanco","negro"))
 
+  def test_tiene_contraste_beige_rosa_es_falso(self):
+    self.assertFalse(tiene_contraste("beige","rosa"))
+
+  def test_tiene_contraste_rojo_azul_es_falso(self):
+    self.assertFalse(tiene_contraste("rojo","azul"))
+
+  def test_tiene_contraste_negro_amarillo_es_verdadero(self):
+    self.assertTrue(tiene_contraste("negro","amarillo"))
